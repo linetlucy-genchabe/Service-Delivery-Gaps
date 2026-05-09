4dceebe3ab22aca37396bfadc3fe4002da671594
Linet  Lucy Genchabe <lgenchabe@livinggoods.org>
2026-05-09 13:26:15 +0300

    Serve service worker at root scope with correct Service-Worker-Allowed header

--- cha_dashboard/urls.py
+++ cha_dashboard/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.http import FileResponse
+import os
+
+def serve_sw(request):
+    sw_path = os.path.join(settings.BASE_DIR, 'dashboard', 'static', 'dashboard', 'sw.js')
+    response = FileResponse(open(sw_path, 'rb'), content_type='application/javascript')
+    response['Service-Worker-Allowed'] = '/'
+    return response
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sw.js', serve_sw, name='service_worker'),
     path('', include('dashboard.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
